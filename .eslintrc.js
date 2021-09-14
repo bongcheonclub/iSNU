5babbac36fe3b505273997828954c8b067e956f8
geongun20 <leesg9@snu.ac.kr>
2021-09-14 15:14:15 +0900

    chore: disable no-unused-vars eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/no-unused-vars': 0,
   },
 };
