3b94f00b0ac7d60d06140b29a48030e8f87bc7a5
geongun20 <leesg9@snu.ac.kr>
2021-09-14 16:14:47 +0900

    feat: use native-base ui kit & mart screen poc

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import {NativeBaseProvider} from 'native-base';
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import Cafe from './screens/Cafe';
@@ -30,54 +31,56 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName={'Meal'}>
-        <Tab.Screen name="Meal" component={Meal} />
-        <Tab.Screen
-          name="Cafe"
-          component={Cafe}
-          // options={{
-          //   tabBarIcon: ({focused, color, size}) => (
-          //     <Icon name="search" size={focused ? '30' : '25'} color="white" />
-          //   ),
-          // }}
-        />
-        <Tab.Screen
-          name="Mart"
-          component={Mart}
-          // options={{
-          //   tabBarIcon: ({focused, color, size}) => (
-          //     <Icon name="upload" size={focused ? '30' : '25'} color="white" />
-          //   ),
-          // }}
-        />
-        <Tab.Screen
-          name="Shuttle"
-          component={Shuttle}
-          // options={{
-          //   tabBarIcon: ({focused, color, size}) => (
-          //     <Icon name="shop" size={focused ? '30' : '25'} color="white" />
-          //   ),
-          // }}
-        />
-        <Tab.Screen
-          name="Etcs"
-          component={Etcs}
-          // options={{
-          //   tabBarIcon: ({focused, color, size}) => (
-          //     <View>
-          //       <Image
-          //         style={
-          //           focused ? style.focusedProfileImage : style.profileImage
-          //         }
-          //         source={ProfileImage}
-          //       />
-          //     </View>
-          //   ),
-          // }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName={'Mart'}>
+          <Tab.Screen name="식당" component={Meal} />
+          <Tab.Screen
+            name="카페"
+            component={Cafe}
+            // options={{
+            //   tabBarIcon: ({focused, color, size}) => (
+            //     <Icon name="search" size={focused ? '30' : '25'} color="white" />
+            //   ),
+            // }}
+          />
+          <Tab.Screen
+            name="편의점"
+            component={Mart}
+            // options={{
+            //   tabBarIcon: ({focused, color, size}) => (
+            //     <Icon name="upload" size={focused ? '30' : '25'} color="white" />
+            //   ),
+            // }}
+          />
+          <Tab.Screen
+            name="셔틀"
+            component={Shuttle}
+            // options={{
+            //   tabBarIcon: ({focused, color, size}) => (
+            //     <Icon name="shop" size={focused ? '30' : '25'} color="white" />
+            //   ),
+            // }}
+          />
+          <Tab.Screen
+            name="기타 편의정보"
+            component={Etcs}
+            // options={{
+            //   tabBarIcon: ({focused, color, size}) => (
+            //     <View>
+            //       <Image
+            //         style={
+            //           focused ? style.focusedProfileImage : style.profileImage
+            //         }
+            //         source={ProfileImage}
+            //       />
+            //     </View>
+            //   ),
+            // }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
--- screens/Mart.tsx
+++ screens/Mart.tsx
@@ -1,13 +1,92 @@
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
+import {Box, Center, HStack, ScrollView, Text, VStack} from 'native-base';
 import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {RootTabList} from '../App';
 
 type Props = BottomTabScreenProps<RootTabList, 'Mart'>;
 
+const DUMMY_MART_INFO: {
+  name: string;
+  location: string;
+  operatingTime: string;
+}[] = [
+  {
+    name: '학생회관 매점',
+    location: '학생회관(63동) 1층',
+    operatingTime: '운영중 19:30 종료',
+  },
+  {
+    name: '학생회관 문구점',
+    location: '학생회관(63동) 2층',
+    operatingTime: '운영중 18:30 종료',
+  },
+  {
+    name: '중도 CU',
+    location: '중도(62동) 3층',
+    operatingTime: '운영중 22:00 종료',
+  },
+  {
+    name: '학생회관 매점',
+    location: '학생회관(63동) 1층',
+    operatingTime: '운영중 19:30 종료',
+  },
+  {
+    name: '학생회관 문구점',
+    location: '학생회관(63동) 2층',
+    operatingTime: '운영중 18:30 종료',
+  },
+  {
+    name: '중도 CU',
+    location: '중도(62동) 3층',
+    operatingTime: '운영중 22:00 종료',
+  },
+  {
+    name: '학생회관 매점',
+    location: '학생회관(63동) 1층',
+    operatingTime: '운영중 19:30 종료',
+  },
+  {
+    name: '학생회관 문구점',
+    location: '학생회관(63동) 2층',
+    operatingTime: '운영중 18:30 종료',
+  },
+  {
+    name: '중도 CU',
+    location: '중도(62동) 3층',
+    operatingTime: '운영중 22:00 종료',
+  },
+  {
+    name: '학생회관 매점',
+    location: '학생회관(63동) 1층',
+    operatingTime: '운영중 19:30 종료',
+  },
+  {
+    name: '학생회관 문구점',
+    location: '학생회관(63동) 2층',
+    operatingTime: '운영중 18:30 종료',
+  },
+  {
+    name: '중도 CU',
+    location: '중도(62동) 3층',
+    operatingTime: '운영중 22:00 종료',
+  },
+];
+
 export default function Mart({navigation}: Props) {
-  // todo
-  return null;
+  return (
+    <ScrollView>
+      <VStack>
+        {DUMMY_MART_INFO.map(({name, location, operatingTime}) => (
+          <Center margin={1} bg="#333333" rounded="md" shadow={3}>
+            <Text color="white">{name}</Text>
+            <Text color="white">{location}</Text>
+            <Text color="white">{operatingTime}</Text>
+          </Center>
+        ))}
+      </VStack>
+    </ScrollView>
+  );
 }
 
 const style = StyleSheet.create({
