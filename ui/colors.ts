6d96ca9b068ebfad6d5e63e931145a7d6680d679
geongun20 <leesg9@snu.ac.kr>
2021-09-19 17:30:06 +0900

    chore: add color palette

---
+++ ui/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  green: '#48D848',
+  grey: {
+    100: '#F8F8F8',
+    200: '#DCDCDC',
+    300: '#929292',
+    400: '#636363',
+  },
+  red: '#F57E64',
+  bage: {100: '#E9E7CE', 200: '#A17C2F'},
+  blue: '#0C146B',
+  black: '#000000',
+};
