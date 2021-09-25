e41886c632a71e5563f3b67da22043e121f9d127
cngustjr1235 <cngustjr1235@snu.ac.kr>
2021-09-25 21:20:02 +0900

    드디어 첫 버튼 만듦

--- screens/Etcs.tsx
+++ screens/Etcs.tsx
@@ -1,15 +1,39 @@
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
+import {Box, Center, Button, ScrollView, VStack, Text} from 'native-base';
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import {Keyboard, StyleSheet} from 'react-native';
 import {RootTabList} from '../App';
+import {colors} from '../ui/colors';
 
 type Props = BottomTabScreenProps<RootTabList, 'Etcs'>;
 
 export default function Etcs({navigation}: Props) {
-  // todo
-  return null;
+  const name = '은행';
+  return (
+    <Box>
+      <ScrollView bgColor={colors.white}>
+        <VStack>
+          <Center>
+            <Button
+              rounded="10px"
+              width="315px"
+              height="72px"
+              bgColor={colors.grey[100]}
+              borderWidth={2}
+              borderColor={colors.grey[200]}>
+              <Text
+                color={colors.grey[400]}
+                // fontFamily="Noto Sans KR"
+                fontSize="25px"
+                fontWeight="500">
+                {name}
+              </Text>
+            </Button>
+          </Center>
+        </VStack>
+      </ScrollView>
+    </Box>
+  );
 }
 
-const style = StyleSheet.create({
-  // todo
-});
+const style = StyleSheet.create({});
