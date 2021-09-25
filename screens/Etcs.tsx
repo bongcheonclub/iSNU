97de1fd23f59946b4df0c8db8eccb8dc9b5c0dd5
cngustjr1235 <cngustjr1235@snu.ac.kr>
2021-09-25 23:05:10 +0900

    다른 버튼도 일단 만듦

--- screens/Etcs.tsx
+++ screens/Etcs.tsx
@@ -1,5 +1,6 @@
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
 import {Box, Center, Button, ScrollView, VStack, Text} from 'native-base';
+import {color} from 'native-base/lib/typescript/theme/styled-system';
 import React, {useState} from 'react';
 import {Keyboard, StyleSheet} from 'react-native';
 import {RootTabList} from '../App';
@@ -8,12 +9,11 @@ import {colors} from '../ui/colors';
 type Props = BottomTabScreenProps<RootTabList, 'Etcs'>;
 
 export default function Etcs({navigation}: Props) {
-  const name = '은행';
   return (
     <Box>
       <ScrollView bgColor={colors.white}>
         <VStack>
-          <Center>
+          <Center margin={2.5}>
             <Button
               rounded="10px"
               width="315px"
@@ -26,7 +26,92 @@ export default function Etcs({navigation}: Props) {
                 // fontFamily="Noto Sans KR"
                 fontSize="25px"
                 fontWeight="500">
-                {name}
+                은행
+              </Text>
+            </Button>
+          </Center>
+          <Center margin={2.5}>
+            <Button
+              rounded="10px"
+              width="315px"
+              height="72px"
+              bgColor={colors.grey[100]}
+              borderWidth={2}
+              borderColor={colors.grey[200]}>
+              <Text
+                color={colors.grey[400]}
+                // fontFamily="Noto Sans KR"
+                fontSize="25px"
+                fontWeight="500">
+                우체국
+              </Text>
+            </Button>
+          </Center>
+          <Center margin={2.5}>
+            <Button
+              rounded="10px"
+              width="315px"
+              height="72px"
+              bgColor={colors.grey[100]}
+              borderWidth={2}
+              borderColor={colors.grey[200]}>
+              <Text
+                color={colors.grey[400]}
+                // fontFamily="Noto Sans KR"
+                fontSize="25px"
+                fontWeight="500">
+                교보문고
+              </Text>
+            </Button>
+          </Center>
+          <Center margin={2.5}>
+            <Button
+              rounded="10px"
+              width="315px"
+              height="72px"
+              bgColor={colors.grey[100]}
+              borderWidth={2}
+              borderColor={colors.grey[200]}>
+              <Text
+                color={colors.grey[400]}
+                // fontFamily="Noto Sans KR"
+                fontSize="25px"
+                fontWeight="500">
+                도서관
+              </Text>
+            </Button>
+          </Center>
+          <Center margin={2.5}>
+            <Button
+              rounded="10px"
+              width="315px"
+              height="72px"
+              bgColor={colors.grey[100]}
+              borderWidth={2}
+              borderColor={colors.grey[200]}>
+              <Text
+                color={colors.grey[400]}
+                // fontFamily="Noto Sans KR"
+                fontSize="25px"
+                fontWeight="500">
+                보건진료소
+              </Text>
+            </Button>
+          </Center>
+          <Center margin={2.5}>
+            <Button
+              rounded="10px"
+              width="315px"
+              height="72px"
+              bgColor={colors.grey[100]}
+              borderWidth={2}
+              borderColor={colors.grey[200]}>
+              <Text
+                color={colors.grey[400]}
+                // fontFamily="Noto Sans KR"
+                fontSize="25px"
+                fontWeight="500">
+                기숙사 편의시설
               </Text>
             </Button>
           </Center>
