95b37b5b5e29ce5861381b6cb87196038916c53c
cngustjr1235 <cngustjr1235@snu.ac.kr>
2021-10-01 23:02:10 +0900

    Color List Added'

--- ui/theme.ts
+++ ui/theme.ts
@@ -16,4 +16,32 @@ export const theme = extendTheme({
     body: 'NotoSansKR',
     mono: 'NotoSansKR',
   },
+
+  colors: {
+    brown: {
+      100: '#E9E7CE', //Favorite Place Background
+      200: '#DDD9A5', //Favorite Place Outline
+      300: '#A17C2F', //Favorite Place Name
+      400: '#8B7A55', //Favorite Menu Price
+      500: '#59584E', //Favorite Menu Name
+    },
+    gray: {
+      100: '#F8F8F8', //Normal Place Background
+      200: '#EBEBEB', //Closed Favorite Place Background
+      300: '#DCDCDC', //Normal Place Outline
+      400: '#ABABAB', //Closed Normal Place Name
+      500: '#929292', //Sub Info
+      600: '#888888', //Favorite Place Time
+      700: '#636363', //Normal Place Name
+    },
+    blue: '#0C146B', //Page Title
+  },
+
+  components: {
+    Text: {
+      baseStyle: {
+        fontFamily: 'body',
+      },
+    },
+  },
 });
