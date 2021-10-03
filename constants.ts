c3602b80cdd643dec04d2b05746f0868e64f4079
geongun20 <leesg9@snu.ac.kr>
2021-10-03 22:57:05 +0900

    chore: move slack webhook url to secure.ts

--- constants.ts
+++ constants.ts
@@ -3,6 +3,3 @@ export const FAVORITE_STORAGE_KEY = {
   mart: 'favoriteMartList',
   shuttle: 'favoriteShuttleList',
 };
-
-export const SLACK_ENDPOINT =
-  'https://hooks.slack.com/services/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';
