6ef219ae671d3d6a1bf05e4a018efcc4cf4ca705
cngustjr1235 <cngustjr1235@snu.ac.kr>
2021-10-04 03:33:47 +0900

    theme 적용

--- screens/Etcs.tsx
+++ screens/Etcs.tsx
@@ -149,16 +149,8 @@ export default function Etcs({navigation}: Props) {
               rounded="10px"
               width="315px"
               height="72px"
-              bgColor={colors.grey[100]}
-              borderWidth={2}
-              borderColor={colors.grey[200]}>
-              <Text
-                color={colors.grey[400]}
-                // fontFamily="Noto Sans KR"
-                fontSize="25px"
-                fontWeight="500">
-                은행
-              </Text>
+              variant="place">
+              <Text variant="normalOpenPlaceBig">은행</Text>
             </Button>
           </Center>
           <Center margin={2.5}>
@@ -167,16 +159,8 @@ export default function Etcs({navigation}: Props) {
               rounded="10px"
               width="315px"
               height="72px"
-              bgColor={colors.grey[100]}
-              borderWidth={2}
-              borderColor={colors.grey[200]}>
-              <Text
-                color={colors.grey[400]}
-                // fontFamily="Noto Sans KR"
-                fontSize="25px"
-                fontWeight="500">
-                우체국
-              </Text>
+              variant="place">
+              <Text variant="normalOpenPlaceBig">우체국</Text>
             </Button>
           </Center>
           <Center margin={2.5}>
@@ -185,16 +169,8 @@ export default function Etcs({navigation}: Props) {
               rounded="10px"
               width="315px"
               height="72px"
-              bgColor={colors.grey[100]}
-              borderWidth={2}
-              borderColor={colors.grey[200]}>
-              <Text
-                color={colors.grey[400]}
-                // fontFamily="Noto Sans KR"
-                fontSize="25px"
-                fontWeight="500">
-                교보문고
-              </Text>
+              variant="place">
+              <Text variant="normalOpenPlaceBig">교보문고</Text>
             </Button>
           </Center>
           <Center margin={2.5}>
@@ -203,16 +179,8 @@ export default function Etcs({navigation}: Props) {
               rounded="10px"
               width="315px"
               height="72px"
-              bgColor={colors.grey[100]}
-              borderWidth={2}
-              borderColor={colors.grey[200]}>
-              <Text
-                color={colors.grey[400]}
-                // fontFamily="Noto Sans KR"
-                fontSize="25px"
-                fontWeight="500">
-                도서관
-              </Text>
+              variant="place">
+              <Text variant="normalOpenPlaceBig">도서관</Text>
             </Button>
           </Center>
           <Center margin={2.5}>
@@ -221,16 +189,8 @@ export default function Etcs({navigation}: Props) {
               rounded="10px"
               width="315px"
               height="72px"
-              bgColor={colors.grey[100]}
-              borderWidth={2}
-              borderColor={colors.grey[200]}>
-              <Text
-                color={colors.grey[400]}
-                // fontFamily="Noto Sans KR"
-                fontSize="25px"
-                fontWeight="500">
-                보건진료소
-              </Text>
+              variant="place">
+              <Text variant="normalOpenPlaceBig">보건진료소</Text>
             </Button>
           </Center>
           <Center margin={2.5}>
@@ -239,16 +199,8 @@ export default function Etcs({navigation}: Props) {
               rounded="10px"
               width="315px"
               height="72px"
-              bgColor={colors.grey[100]}
-              borderWidth={2}
-              borderColor={colors.grey[200]}>
-              <Text
-                color={colors.grey[400]}
-                // fontFamily="Noto Sans KR"
-                fontSize="25px"
-                fontWeight="500">
-                기숙사 편의시설
-              </Text>
+              variant="place">
+              <Text variant="normalOpenPlaceBig">기숙사 편의시설</Text>
             </Button>
           </Center>
         </VStack>
