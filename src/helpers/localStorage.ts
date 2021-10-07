267884de903f7437b74894d312c27f901edaff01
Dojunggeun <jg.do@snu.ac.kr>
2021-10-07 23:24:34 +0900

    식당 기본즐찾(학생회관) 설정

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -16,6 +16,14 @@ export type LOCAL_STORAGE = {
 export async function getItem<T extends keyof LOCAL_STORAGE>(
   key: T,
 ): Promise<LOCAL_STORAGE[T] | null> {
+  if (key === 'favoriteMeals') {
+    const item = await AsyncStorage.getItem(key);
+    const parsedItem =
+      item != null
+        ? (JSON.parse(item) as LOCAL_STORAGE[T])
+        : (['학생회관'] as LOCAL_STORAGE[T]);
+    return parsedItem;
+  }
   const item = await AsyncStorage.getItem(key);
   const parsedItem =
     item != null ? (JSON.parse(item) as LOCAL_STORAGE[T]) : null;
