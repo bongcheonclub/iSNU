ef4d13625ea4faa41ac290c0cf4855bfaeaaa228
cngustjr1235 <cngustjr1235@snu.ac.kr>
2021-10-09 22:47:54 +0900

    Delete flex (typo)

--- src/screens/Cafe.tsx
+++ src/screens/Cafe.tsx
@@ -69,7 +69,7 @@ function checkOperating(cafe: Cafe): boolean {
   return true;
 }
 
-export default function Cafe({flex, cafes, initialFavoriteNames}: Props) {
+export default function Cafe({cafes, initialFavoriteNames}: Props) {
   return (
     <Grid
       itemType="cafe"
