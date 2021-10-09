d0df94fc700c5b7841e1cf71417e6bb9537595b8
geongun20 <leesg9@snu.ac.kr>
2021-10-09 12:24:56 +0900

    chore: add default mart

--- src/InitializeData/ProcessOthersData.ts
+++ src/InitializeData/ProcessOthersData.ts
@@ -29,7 +29,46 @@ export function processMartData(res: AxiosResponse<any>) {
       };
     })
     .value();
-  return marts;
+
+  const defaultMart = [
+    {
+      name: '해동 학술관',
+      location: '공과대학 해동학술관 (32-1동 지하1층)',
+      items: '',
+      weekday: '08:00~23:00',
+      saturday: '08:00~23:00',
+      holiday: '08:00~23:00',
+      contact: '',
+    },
+    {
+      name: '34동',
+      location: '공과대학 (34동 1층)',
+      items: '',
+      weekday: '24시간',
+      saturday: '24시간',
+      holiday: '24시간',
+      contact: '',
+    },
+    {
+      name: '사회대',
+      location: '사회과학대학 신양학술정보관 (16-1동 1층)',
+      items: '',
+      weekday: '24시간',
+      saturday: '24시간',
+      holiday: '24시간',
+      contact: '',
+    },
+    {
+      name: '기숙사GS25',
+      location: '대학원 기숙사 (901동 1층)',
+      items: '',
+      weekday: '24시간',
+      saturday: '24시간',
+      holiday: '24시간',
+      contact: '',
+    },
+  ];
+  return marts.concat(defaultMart);
 }
 
 export function processCafeData(res: AxiosResponse<any>) {
