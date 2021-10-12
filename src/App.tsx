3bc6ae984a63db609b631b5da77d05bae5a8f0d8
Dojunggeun <jg.do@snu.ac.kr>
2021-10-12 21:42:17 +0900

    add alert for new user (about covid19)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {Box, Flex, NativeBaseProvider, HStack, StatusBar} from 'native-base';
+import {
+  Box,
+  Flex,
+  NativeBaseProvider,
+  HStack,
+  StatusBar,
+  Alert,
+  VStack,
+  IconButton,
+  CloseIcon,
+} from 'native-base';
 import React, {useCallback, useEffect, useState} from 'react';
 import {SafeAreaView, Dimensions, Platform} from 'react-native';
 import Cafe from './screens/Cafe';
@@ -23,6 +33,7 @@ import MoreModal from './components/MoreModal';
 import Text from './components/Text';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Tab = createBottomTabNavigator();
 
@@ -217,7 +228,74 @@ export default function App() {
             </NavigationContainer>
           </>
         ) : null}
+        <FirstAlert />
       </Box>
     </NativeBaseProvider>
   );
 }
+
+const FirstAlert = () => {
+  const [displayNotice, setDisplayNotice] = useState(false);
+  useEffect(() => {
+    (async () => {
+      const alreadyViewNotice = await AsyncStorage.getItem('wasViewedNotice');
+      if (alreadyViewNotice !== 'true') {
+        setDisplayNotice(true);
+      }
+    })();
+  }, []);
+
+  if (displayNotice === true) {
+    return (
+      <Alert
+        w="90%"
+        status="info"
+        colorScheme="info"
+        marginX="5%"
+        marginBottom={Platform.OS === 'android' ? '70px' : '100px'}
+        bottom="0px"
+        shadow={9}
+        position="absolute">
+        <VStack
+          padding="2%"
+          space={2}
+          flexShrink={1}
+          w="100%"
+          alignItems="center"
+          justifyContent="center">
+          <HStack
+            w="100%"
+            flexShrink={1}
+            space={2}
+            alignItems="center"
+            justifyContent="space-between">
+            <Alert.Icon size="5" />
+            <Text fontSize="md" fontWeight="medium" color="coolGray.800">
+              코로나19 관련 운영시간 변동 안내
+            </Text>
+            <IconButton
+              onPress={() => {
+                setDisplayNotice(false);
+                AsyncStorage.setItem('wasViewedNotice', 'true');
+              }}
+              variant="unstyled"
+              icon={<CloseIcon size="3" color="coolGray.600" />}
+            />
+          </HStack>
+          <Box w="100%">
+            <Text color="coolGray.600" fontSize="16px" lineHeight="lg">
+              {'\n'}모든 정보는 생활협동조합 홈페이지, 마이스누 포털 등을
+              바탕으로 바탕으로 제공됩니다. {'\n\n'}다만 코로나19 상황에 따라 각
+              매장의 실제 운영시간에 변동이 있을 수 있습니다.
+              {'\n\n'}혹시 잘못된 정보를 확인하신 분들은 제보하기 기능을 통해
+              제보해주시면 감사하겠습니다.{'\n\n'}저희도 앞으로 더 정확한 정보를
+              제공하기 위하여 노력하겠습니다. 감사합니다.
+            </Text>
+          </Box>
+        </VStack>
+      </Alert>
+    );
+  } else {
+    return <></>;
+  }
+};
