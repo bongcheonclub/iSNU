fbfa3cdcc62c421b7825ebe3ee36fb40acb6650f
Dojunggeun <jg.do@snu.ac.kr>
2021-11-05 22:29:46 +0900

    리뷰 일부만 반영 및 변수명 일부 변경

--- src/InitializeData/FetchData.ts
+++ src/InitializeData/FetchData.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import {getDate, getMonth, getYear} from 'date-fns';
+import {addDays, getDate, getMonth, getYear} from 'date-fns';
 import {getItem} from '../helpers/localStorage';
 
 export async function fetchLocalStoreageData() {
@@ -33,49 +33,44 @@ export async function fetchLocalStoreageData() {
 
 export async function fetchCrawlData() {
   // const now = new Date('Tue Oct 26 2021 12:24:15 GMT+0900');
-  const now = new Date();
-  const year = getYear(now);
-  const month = getMonth(now) + 1;
-  const date = getDate(now);
-  const day0MenuURL = `https://snuco.snu.ac.kr/ko/foodmenu?field_menu_date_value_1%5Bvalue%5D%5Bdate%5D=&field_menu_date_value%5Bvalue%5D%5Bdate%5D=${month}%2F${date}%2F${year}`;
-
-  function getDateMenuURL(
-    thisYear: number,
-    thisMonth: number,
-    thisDate: number,
-    offset: number,
-  ): string {
-    const _day = new Date(thisYear, thisMonth - 1, thisDate + offset);
-    const _year = getYear(_day);
-    const _month = getMonth(_day) + 1;
-    const _date = getDate(_day);
-    const _url = `https://snuco.snu.ac.kr/ko/foodmenu?field_menu_date_value_1%5Bvalue%5D%5Bdate%5D=&field_menu_date_value%5Bvalue%5D%5Bdate%5D=${_month}%2F${_date}%2F${_year}`;
-    return _url;
+  const DATE_OFFSETS = [-3, -2, -1, 0, 1, 2, 3];
+  function getDateMenuURL(offset: number): string {
+    const now = new Date();
+    const date = addDays(now, offset);
+    const url = `https://snuco.snu.ac.kr/ko/foodmenu?field_menu_date_value_1%5Bvalue%5D%5Bdate%5D=&field_menu_date_value%5Bvalue%5D%5Bdate%5D=${getMonth(
+      date,
+    )}%2F${getDate(date)}%2F${getYear(date)}`;
+    return url;
   }
-  const day1MenuURL = getDateMenuURL(year, month, date, 1);
-  const day2MenuURL = getDateMenuURL(year, month, date, 2);
-  const day_1MenuURL = getDateMenuURL(year, month, date, -1);
-  const day_2MenuURL = getDateMenuURL(year, month, date, -2);
+  const menuUrlsWithOffset = DATE_OFFSETS.map(offset => ({
+    url: getDateMenuURL(offset),
+    offset,
+  }));
+
   const [
     cafeRes,
     martRes,
     mealListRes,
     mealDormListRes,
-    day0MenuListRes,
-    day1MenuListRes,
-    day2MenuListRes,
-    day_1MenuListRes,
-    day_2MenuListRes,
+    [
+      day0MenuListRes,
+      dayAfter1MenuListRes,
+      dayAfter2MenuListRes,
+      dayBefore1MenuListRes,
+      dayBefore2MenuListRes,
+    ],
+    ,
   ] = await Promise.all([
     axios.get('https://snuco.snu.ac.kr/ko/node/21'),
     axios.get('https://snuco.snu.ac.kr/ko/node/19'),
     axios.get('https://snuco.snu.ac.kr/ko/node/20'),
     axios.get('https://snudorm.snu.ac.kr/food-schedule/'),
-    axios.get(day0MenuURL),
-    axios.get(day1MenuURL),
-    axios.get(day2MenuURL),
-    axios.get(day_1MenuURL),
-    axios.get(day_2MenuURL),
+    Promise.all(
+      menuUrlsWithOffset.map(async ({offset, url}) => {
+        const res = await axios.get(url);
+        return res; // offset 혹은 연월일 리턴해서 활용하기
+      }),
+    ),
   ]);
   return {
     cafeRes,
@@ -83,9 +78,9 @@ export async function fetchCrawlData() {
     mealListRes,
     mealDormListRes,
     day0MenuListRes,
-    day1MenuListRes,
-    day2MenuListRes,
-    day_1MenuListRes,
-    day_2MenuListRes,
+    dayAfter1MenuListRes,
+    dayAfter2MenuListRes,
+    dayBefore1MenuListRes,
+    dayBefore2MenuListRes,
   };
 }
