aa89d77a3eb1f0ef3052330fb1261d38184aeba8
Dojunggeun <jg.do@snu.ac.kr>
2021-11-05 22:41:51 +0900

    fix getmonth (+1해줌) 및 리뷰 마저 반영..

--- src/InitializeData/FetchData.ts
+++ src/InitializeData/FetchData.ts
@@ -4,7 +4,7 @@ import {addDays, getDate, getMonth, getYear} from 'date-fns';
 import {getItem} from '../helpers/localStorage';
 
 export async function fetchLocalStoreageData() {
-  // AsyncStorage.clear(); // 개발용 코드
+  AsyncStorage.clear(); // 개발용 코드
   const [
     favoriteCafes,
     favoriteMarts,
@@ -37,9 +37,9 @@ export async function fetchCrawlData() {
   function getDateMenuURL(offset: number): string {
     const now = new Date();
     const date = addDays(now, offset);
-    const url = `https://snuco.snu.ac.kr/ko/foodmenu?field_menu_date_value_1%5Bvalue%5D%5Bdate%5D=&field_menu_date_value%5Bvalue%5D%5Bdate%5D=${getMonth(
-      date,
-    )}%2F${getDate(date)}%2F${getYear(date)}`;
+    const url = `https://snuco.snu.ac.kr/ko/foodmenu?field_menu_date_value_1%5Bvalue%5D%5Bdate%5D=&field_menu_date_value%5Bvalue%5D%5Bdate%5D=${
+      getMonth(date) + 1
+    }%2F${getDate(date)}%2F${getYear(date)}`;
     return url;
   }
   const menuUrlsWithOffset = DATE_OFFSETS.map(offset => ({
@@ -53,11 +53,11 @@ export async function fetchCrawlData() {
     mealListRes,
     mealDormListRes,
     [
+      dayBefore2MenuListRes,
+      dayBefore1MenuListRes,
       day0MenuListRes,
       dayAfter1MenuListRes,
       dayAfter2MenuListRes,
-      dayBefore1MenuListRes,
-      dayBefore2MenuListRes,
     ],
     ,
   ] = await Promise.all([
@@ -65,7 +65,7 @@ export async function fetchCrawlData() {
     axios.get('https://snuco.snu.ac.kr/ko/node/19'),
     axios.get('https://snuco.snu.ac.kr/ko/node/20'),
     axios.get('https://snudorm.snu.ac.kr/food-schedule/'),
-    Promise.all(
+    await Promise.all(
       menuUrlsWithOffset.map(async ({offset, url}) => {
         const res = await axios.get(url);
         return res; // offset 혹은 연월일 리턴해서 활용하기
