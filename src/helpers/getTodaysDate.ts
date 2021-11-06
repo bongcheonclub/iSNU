1d6e773858797e5d51ed0b4c1b982dfbaaea6c1a
Dojunggeun <jg.do@snu.ac.kr>
2021-11-06 15:20:49 +0900

    helper convertToKoreanDay 작성

--- src/helpers/getTodaysDate.ts
+++ src/helpers/getTodaysDate.ts
@@ -1,4 +1,5 @@
 import {getDate, getDay, getMonth, getYear} from 'date-fns';
+import {convertToKoreanDay} from './convertToKoreanDay';
 import {getNow} from './getNow';
 
 export function getTodaysDate() {
@@ -7,29 +8,6 @@ export function getTodaysDate() {
   const date = getDate(nowDate);
   const day = getDay(nowDate);
   const year = getYear(nowDate);
-  const koreanDay = (() => {
-    if (day === 0) {
-      return '일';
-    }
-    if (day === 1) {
-      return '월';
-    }
-    if (day === 2) {
-      return '화';
-    }
-    if (day === 3) {
-      return '수';
-    }
-    if (day === 4) {
-      return '목';
-    }
-    if (day === 5) {
-      return '금';
-    }
-    if (day === 6) {
-      return '토';
-    }
-    throw Error('이럴리없다.');
-  })();
+  const koreanDay = convertToKoreanDay(day);
   return {year, month, date, koreanDay, day};
 }
