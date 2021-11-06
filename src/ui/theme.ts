e7fcca7dad15e4dea23848f23ce776e0cfa8821b
geongun20 <leesg9@snu.ac.kr>
2021-11-06 14:25:49 +0900

    fix: 용량 줄이기 & 엔트리 파일 설정

--- src/ui/theme.ts
+++ src/ui/theme.ts
@@ -3,12 +3,9 @@ import {extendTheme} from 'native-base';
 export const theme = extendTheme({
   fontConfig: {
     NotoSansKR: {
-      100: 'NotoSansKR-Thin',
-      200: 'NotoSansKR-Light',
       300: 'NotoSansKR-Regular',
       400: 'NotoSansKR-Medium',
       500: 'NotoSansKR-Bold',
-      600: 'NotoSansKR-Black',
     },
   },
   fonts: {
