f560ce0b37e9a835519f3ee071e0f09f63da6bd7
Dojunggeun <jg.do@snu.ac.kr>
2021-12-20 01:48:23 +0900

    셔틀 방학 운영시간 임시 반영 (1/26 이후 운영시간 미반영)

--- src/screens/Shuttle.tsx
+++ src/screens/Shuttle.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {compareAsc, getDay, parse as parseTime} from 'date-fns';
 import List from '../components/List';
 import {getNow} from '../helpers/getNow';
+import {isVacation} from '../InitializeData/ProcessVacation';
 
 type Props = {
   initialFavoriteNames: string[];
@@ -301,6 +302,241 @@ const SHUTTLES: ShuttleType[] = [
   },
 ];
 
+const SHUTTLES_VACATION: ShuttleType[] = [
+  {
+    name: '설입 ↔ 행정관',
+    operatings: [
+      {
+        time: '00:00~07:00',
+        interval: null,
+        numbers: null,
+        partition: '07:00 운행 예정',
+      },
+      {
+        time: '07:00~08:00',
+        interval: '15분',
+        numbers: '2대',
+        partition: '등교 ~18:00, 하교 ~19:00',
+      },
+      {
+        time: '08:00~11:00',
+        interval: '3~4분',
+        numbers: '8대',
+        partition: '등교 ~18:00, 하교 ~19:00',
+      },
+      {
+        time: '11:00~15:00',
+        interval: '10분',
+        numbers: '3대',
+        partition: '등교 ~18:00, 하교 ~19:00',
+      },
+      {
+        time: '15:00~18:00',
+        interval: '3~4분',
+        numbers: '8대',
+        partition: '등교 ~18:00, 하교 ~19:00',
+      },
+      {
+        time: '18:00~19:00',
+        interval: '3~4분\n(하교셔틀만 운영)',
+        numbers: '8대',
+        partition: '하교 ~19:00',
+      },
+      {
+        time: '19:00~21:10',
+        interval: null,
+        numbers: null,
+        partition: '21:10 운행 예정',
+      },
+      {
+        time: '21:10~23:10',
+        interval: '30분',
+        numbers: '2대',
+        partition: '23:10까지 운행',
+      },
+      {
+        time: '23:10~23:59',
+        interval: null,
+        numbers: null,
+        partition: '운행 종료',
+      },
+    ],
+  },
+  {
+    name: '녹두 ↔ 행정관',
+    operatings: [
+      {
+        time: '00:00~07:00',
+        interval: null,
+        numbers: null,
+        partition: '07:00 운행 예정',
+      },
+      {
+        time: '07:00~08:00',
+        interval: '15분',
+        numbers: '1대',
+        partition: '19:00까지 운행',
+      },
+      {
+        time: '08:00~11:00',
+        interval: '6분',
+        numbers: '3대',
+        partition: '19:00까지 운행',
+      },
+      {
+        time: '11:00~19:00',
+        interval: '10분',
+        numbers: '2대',
+        partition: '19:00까지 운행',
+      },
+      {
+        time: '19:00~21:10',
+        interval: null,
+        numbers: null,
+        partition: '21:10 운행 예정',
+      },
+      {
+        time: '21:10~23:10',
+        interval: '30분',
+        numbers: '2대',
+        partition: '23:10까지 운행',
+      },
+      {
+        time: '23:10~23:59',
+        interval: null,
+        numbers: null,
+        partition: '운행 종료',
+      },
+    ],
+  },
+  {
+    name: '사당 ↔ 행정관',
+    operatings: [
+      {
+        time: '00:00~08:00',
+        interval: null,
+        numbers: null,
+        partition: '08:00 운행 예정',
+      },
+      {
+        time: '08:00~11:00',
+        interval: '15분',
+        numbers: '3대',
+        partition: '11:00까지 운행',
+      },
+      {
+        time: '11:00~23:59',
+        interval: null,
+        numbers: null,
+        partition: '운행 종료',
+      },
+    ],
+  },
+  {
+    name: '설입 → 윗공대',
+    operatings: [
+      {
+        time: '00:00~23:59',
+        interval: null,
+        numbers: null,
+        partition: '운행 없음',
+      },
+    ],
+  },
+  {
+    name: '낙성대 → 윗공대',
+    operatings: [
+      {
+        time: '00:00~23:59',
+        interval: null,
+        numbers: null,
+        partition: '운행 없음',
+      },
+    ],
+  },
+  {
+    name: '교내 순환',
+    operatings: [
+      {
+        time: '00:00~08:00',
+        interval: null,
+        numbers: null,
+        partition: '08:00 운행 예정',
+      },
+      {
+        time: '08:00~18:00',
+        interval: '5~6분',
+        numbers: '3~4대',
+        partition: '18:00까지 운행',
+      },
+      {
+        time: '18:00~23:59',
+        interval: null,
+        numbers: null,
+        partition: '운행 종료',
+      },
+    ],
+  },
+  {
+    name: '교내 역순환',
+    operatings: [
+      {
+        time: '00:00~09:50',
+        interval: null,
+        numbers: null,
+        partition: '09:50 운행 예정',
+      },
+      {
+        time: '09:50~12:50',
+        interval: '20분',
+        numbers: '2대',
+        partition: '17:10까지 운행',
+      },
+      {
+        time: '12:50~13:30',
+        interval: null,
+        numbers: null,
+        partition: '13:30 운행 예정',
+      },
+      {
+        time: '13:30~15:10',
+        interval: '40~60분',
+        numbers: '2대',
+        partition: '17:10까지 운행',
+      },
+      {
+        time: '15:10~15:30',
+        interval: null,
+        numbers: null,
+        partition: '15:30부터 ',
+      },
+      {
+        time: '15:30~17:10',
+        interval: '20분',
+        numbers: '2대',
+        partition: '17:10까지 운행',
+      },
+      {
+        time: '17:10~23:59',
+        interval: null,
+        numbers: null,
+        partition: '운행 종료',
+      },
+    ],
+  },
+  {
+    name: '심야 셔틀',
+    operatings: [
+      {
+        time: '00:00~23:59',
+        interval: null,
+        numbers: null,
+        partition: '운행 없음',
+      },
+    ],
+  },
+];
+
 function checkOperating(shuttle: ShuttleType):
   | {
       isOperating: true;
@@ -351,6 +587,17 @@ function checkOperating(shuttle: ShuttleType):
 }
 
 export default function Shuttle({initialFavoriteNames}: Props) {
+  if (isVacation()) {
+    return (
+      <List
+        itemType="shuttle"
+        items={SHUTTLES_VACATION}
+        checkOperating={checkOperating}
+        initialFavoriteNames={initialFavoriteNames}
+        favoriteStorageKey={'favoriteShuttles'}
+      />
+    );
+  }
   return (
     <List
       itemType="shuttle"
